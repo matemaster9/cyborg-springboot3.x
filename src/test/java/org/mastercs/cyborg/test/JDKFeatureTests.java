79891c1ed590656201aa0cc040d0cbab06237924
syler <matemaster@163.com>
2023-06-02 09:37:53 +0800

    日志配置修改

--- src/test/java/org/mastercs/cyborg/test/JDKFeatureTests.java
+++ src/test/java/org/mastercs/cyborg/test/JDKFeatureTests.java
@@ -1,4 +1,25 @@
 package org.mastercs.cyborg.test;
 
+import org.junit.jupiter.api.Test;
+
 public class JDKFeatureTests {
+
+    @Test
+    void name() {
+        String sql = """
+                SELECT
+                    users.user_id,
+                    users.username,
+                    orders.order_id,
+                    orders.order_date,
+                    products.product_id,
+                    products.product_name
+                FROM
+                    users
+                    INNER JOIN orders ON users.user_id = orders.user_id
+                    INNER JOIN order_items ON orders.order_id = order_items.order_id
+                    INNER JOIN products ON order_items.product_id = products.product_id
+                """;
+        System.out.println(sql);
+    }
 }
